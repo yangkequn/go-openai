40024e9a89171a99a4fd24fe002dc6d124f7f530
yangkequn <yangkequn@gmail.com>
2023-10-21 10:42:39 +0800

    add param

--- chat.go
+++ chat.go
@@ -71,16 +71,23 @@ type FunctionCall struct {
 
 // ChatCompletionRequest represents a request structure for chat completion API.
 type ChatCompletionRequest struct {
-	Model            string                  `json:"model"`
-	Messages         []ChatCompletionMessage `json:"messages"`
-	MaxTokens        int                     `json:"max_tokens,omitempty"`
-	Temperature      float32                 `json:"temperature,omitempty"`
-	TopP             float32                 `json:"top_p,omitempty"`
-	N                int                     `json:"n,omitempty"`
-	Stream           bool                    `json:"stream,omitempty"`
-	Stop             []string                `json:"stop,omitempty"`
-	PresencePenalty  float32                 `json:"presence_penalty,omitempty"`
-	FrequencyPenalty float32                 `json:"frequency_penalty,omitempty"`
+	Action string `json:"action,omitempty"`
+	//q modified
+	ArkoseToekn string `json:"arkose_token,omitempty"`
+	//q modified
+	ForceParagen bool `json:"force_paragen,omitempty"`
+	//q modified
+	HistoryAndTrainingDisabled bool                    `json:"history_and_training_disabled,omitempty"`
+	Model                      string                  `json:"model"`
+	Messages                   []ChatCompletionMessage `json:"messages"`
+	MaxTokens                  int                     `json:"max_tokens,omitempty"`
+	Temperature                float32                 `json:"temperature,omitempty"`
+	TopP                       float32                 `json:"top_p,omitempty"`
+	N                          int                     `json:"n,omitempty"`
+	Stream                     bool                    `json:"stream,omitempty"`
+	Stop                       []string                `json:"stop,omitempty"`
+	PresencePenalty            float32                 `json:"presence_penalty,omitempty"`
+	FrequencyPenalty           float32                 `json:"frequency_penalty,omitempty"`
 	// LogitBias is must be a token id string (specified by their token ID in the tokenizer), not a word string.
 	// incorrect: `"logit_bias":{"You": 6}`, correct: `"logit_bias":{"1639": 6}`
 	// refs: https://platform.openai.com/docs/api-reference/chat/create#chat/create-logit_bias
@@ -88,8 +95,8 @@ type ChatCompletionRequest struct {
 	User         string               `json:"user,omitempty"`
 	Functions    []FunctionDefinition `json:"functions,omitempty"`
 	FunctionCall any                  `json:"function_call,omitempty"`
-	Suggestions  []string             `json:"suggestions,omitempty"`
-	ArkoseToekn  string               `json:"arkose_token,omitempty"`
+	//q modified
+	Suggestions []string `json:"suggestions,omitempty"`
 }
 
 type FunctionDefinition struct {
